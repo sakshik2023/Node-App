e186eccfcb8b00c940ebedde5f85196d2e3f6d04
Sakshi71125 <sakshikumari71425@gmail.com>
2026-05-06 15:29:24 +0530

    index file changed

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app=express();
 
 const PORT=3000;
 app.length('/',(req,res)=>{
-    res.send('Helo World')
+    res.send('Hello sakshi')
 })
+
 
 app.listen(PORT,()=>{
     console.log('serverbis running on http://localhost:${PORT}');
